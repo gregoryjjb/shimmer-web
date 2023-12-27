1dedef41f28b9644b847f76b1b5d86d549c251f2
Greg Brisebois <gregoryjjb@gmail.com>
2023-12-26 16:47:06 -0800

    Better looking help

--- src/Help.tsx
+++ src/Help.tsx
@@ -1,13 +1,30 @@
-import { Component } from 'solid-js';
+import { Component, ParentComponent } from 'solid-js';
+
+const Key: ParentComponent = (props) => {
+  return (
+    <p class="rounded border border-zinc-500 px-1 py-0.5 text-xs">
+      {props.children}
+    </p>
+  );
+};
 
 const Keybind: Component<{
   action: string;
   key: string;
+  shift?: boolean;
+  ctrl?: boolean;
+  alt?: boolean;
 }> = (props) => {
   return (
-    <p>
-      <span>{props.action}</span>: {props.key}
-    </p>
+    <div class="rounded bg-zinc-800 p-2">
+      <p class="mb-1 text-sm font-semibold">{props.action}</p>
+      <div class="flex flex-row gap-1">
+        {props.shift && <Key>SHIFT</Key>}
+        {props.ctrl && <Key>CTRL</Key>}
+        {props.alt && <Key>ALT</Key>}
+        <Key>{props.key}</Key>
+      </div>
+    </div>
   );
 };
 
@@ -15,22 +32,32 @@ const Help: Component<{
   onClose?: () => void;
 }> = (props) => {
   return (
-    <div class="px-3 py-2">
-      <p class="text-xl font-semibold">Help</p>
-      <button onClick={() => props.onClose?.()}>Close</button>
-      <Keybind action="Play/pause" key="Spacebar" />
-      <Keybind action="Insert keyframes" key="Right click" />
-      <Keybind action="Insert full row" key="Shift right click" />
-      <Keybind action="Select keyframes" key="Left click" />
-      <Keybind action="Multi-select" key="Shift left click" />
-      <Keybind action="Invert" key="i" />
-      <Keybind action="Move up/down" key="Up/down arrows" />
-      <Keybind action="Grab & move" key="g" />
-      <Keybind action="Scale" key="s" />
-      <Keybind action="Duplicate" key="ctrl-d" />
-      <Keybind action="Delete" key="ctrl-d" />
-      <Keybind action="Remove duplicates" key="w" />
-      <Keybind action="Align/squish" key="a" />
+    <div class="flex flex-col px-3 py-2">
+      <div class="mb-3 flex">
+        <p class="mr-4 text-xl font-semibold">Help</p>
+        <button
+          class="rounded bg-yellow-700 px-2 py-1 text-sm font-semibold text-yellow-100"
+          onClick={() => props.onClose?.()}
+        >
+          Hide
+        </button>
+      </div>
+      <div class="flex flex-row flex-wrap gap-3">
+        <Keybind action="Play/pause" key="Space" />
+        <Keybind action="Insert keyframes" key="RMB" />
+        <Keybind action="Insert full row" ctrl key="RMB" />
+        <Keybind action="Select keyframes" key="LMB" />
+        <Keybind action="Multi-select" shift key="LMB" />
+        <Keybind action="Invert keyframe values" key="I" />
+        <Keybind action="Move up/down" key="Up/down arrows" />
+        <Keybind action="Grab & move" key="G" />
+        <Keybind action="Scale" key="S" />
+        <Keybind action="Duplicate" ctrl key="D" />
+        <Keybind action="Delete" key="DEL" />
+        <Keybind action="Remove duplicates" key="W" />
+        <Keybind action="Align/squish" key="A" />
+        <Keybind action="Equally space" key="E" />
+      </div>
     </div>
   );
 };
