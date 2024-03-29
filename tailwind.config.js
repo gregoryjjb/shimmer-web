c67ef3b591626c4c95c1b1a7b6683100b92ad83b
Greg Brisebois <gregoryjjb@gmail.com>
2024-03-29 13:44:42 -0700

    Rubik font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 const plugin = require('tailwindcss/plugin');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["'Rubik'", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [
     plugin(function ({ addVariant }) {
